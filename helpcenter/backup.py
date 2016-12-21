6be3c6e3b23b5c8964e903101cbf33223ad8c72e
Bianca Rosa <bmendonca@stone.com.br>
2016-12-20 22:34:58 -0200

    cria csv...

--- helpcenter/backup.py
+++ helpcenter/backup.py
@@ -29,24 +29,11 @@ zendesk = 'https://portalstone.zendesk.com'
 language = 'pt-br'
 
 date = datetime.date.today()
-backup_path = os.path.join('backup', str(date), language)
-
+backup_path = 'backup'
+backup_images = False
 log = []
 image_urls = []
-sections = {}
-endpoint = zendesk + \
-    '/api/v2/help_center/{locale}/sections.json'.format(
-        locale=language.lower())
-while endpoint:
-    response = session.get(endpoint)
-    if response.status_code != 200:
-        log.append('Failed to retrieve sections with error {}. URL: {}'.format(
-            response.status_code, endpoint))
-    data = response.json()
-    for section in data['sections']:
-        sections[section['id']] = section['name']
-    endpoint = data['next_page']
-
+data_csv = []
 endpoint = zendesk + \
     '/api/v2/help_center/{locale}/articles.json'.format(
         locale=language.lower())
@@ -58,40 +45,35 @@ while endpoint:
     data = response.json()
 
     for article in data['articles']:
+        print('----Article-----')
+        print(article['id'])
         title = '<h1>' + article['title'] + '</h1>'
         name = article['name'].encode('ascii', 'ignore').decode(
             'ascii').replace('/', ' ').strip().replace('?', '').replace(
             '"',"").replace("*", '').replace(':','')
-        filename = '{name}.html'.format(name=name)
+        filename = '{article}.html'.format(article=article['id'])
         sid = article['section_id']
-        section_name = sections.get(sid, sid).encode(
-            'ascii', 'ignore').decode('ascii').replace(
-                '/', ' ').strip().replace('?', '').replace(
-            '"',"").replace("*", '').replace(':','')
-        path = os.path.join(
-            backup_path, '{section}'.format(section=section_name),
-            '{article}'.format(article=article['id']))
 
-        if not os.path.exists(path):
-            os.makedirs(path)
-            if article['draft'] is True:
-                log.append('{id} is draft!'.format(id=article['id']))
-            elif article['body'] is None:
-                log.append('{id} not copied!'.format(id=article['id']))
-            else:
+        if not os.path.exists(backup_path):
+            os.makedirs(backup_path)
+        if article['draft'] is True:
+            log.append('{id} is draft!'.format(id=article['id']))
+        elif article['body'] is None:
+            log.append('{id} not copied!'.format(id=article['id']))
+        else:
+            body = article['body']
+
+            if backup_images:
                 parser = MyHTMLParser()
                 parser.feed(article['body'])
                 image_urls.append(parser.urls)
 
                 count = 0
-                images_path = os.path.join(path, 'images')
+                images_path = os.path.join(backup_path, 'images')
                 if len(parser.urls) > 0:
                     if not os.path.exists(images_path):
                         os.makedirs(images_path)
 
-                print('----Article-----')
-                print(article['id'])
-                body = article['body']
                 for url in parser.urls:
                     try:
                         count += 1
@@ -111,10 +93,13 @@ while endpoint:
                         log.append('Couldnt get picture url {url} for article {article}'.format(
                             url=url, article=article['id']))
 
-                f = open(os.path.join(path, filename), 'w', encoding='utf-8')
-                f.write(title + '\n')
-                f.write(body)
-                f.close()
+            fname = os.path.join(backup_path, filename)
+            f = open(fname, 'w', encoding='utf-8')
+            f.write(title + '\n')
+            f.write(body)
+            f.close()
+            log.append('{id} copied!'.format(id=article['id']))
+            data_csv.append(",".join([fname, article['title'], str(sid)]))
 
     endpoint = data['next_page']
 
@@ -126,7 +111,13 @@ for url in image_urls:
     f.write(url + '\n')
 f.close()
 
-f = open(os.path.join(backup_path, 'log.txt', 'w', encoding='utf-8'))
-for log in logs:
-    f.write(log + '\n')
+f = open(os.path.join(backup_path, 'log.txt'), 'w', encoding='utf-8')
+for line in log:
+    f.write(unicode(line + "\n"))
+f.close()
+
+f = open(os.path.join(backup_path, 'data.csv'), 'w', encoding='utf-8')
+f.write(unicode("Filename,Title,DataCategoryId\n"))
+for line in data_csv:
+    f.write(unicode(line + "\n"))
 f.close()
